9e53db32890b54ab5b5e26b27c6eb2672c0c05a6
Peter Ombwa <peter.ombwa@microsoft.com>
2021-11-01 16:31:26 -0700

    Make regex static and compiled.

--- tools/Custom/HttpMessageLogFormatter.cs
+++ tools/Custom/HttpMessageLogFormatter.cs
@@ -17,6 +17,7 @@ namespace Microsoft.Graph.PowerShell
 
     public static class HttpMessageLogFormatter
     {
+        private static Regex regexPattern = new Regex("(\\s*\"access_token\"\\s*:\\s*)\"[^\"]+\"", RegexOptions.Compiled);
         public static string GetHttpRequestLog(HttpRequestMessage request)
         {
             if (request == null) return string.Empty;
@@ -24,7 +25,7 @@ namespace Microsoft.Graph.PowerShell
             string body = string.Empty;
             try
             {
-                body = FormatString(request.Content.ReadAsStringAsync().Result);
+                body = (request.Content == null) ? string.Empty : FormatString(request.Content.ReadAsStringAsync().Result);
             }
             catch { }
 
@@ -44,7 +45,7 @@ namespace Microsoft.Graph.PowerShell
             string body = string.Empty;
             try
             {
-                body = FormatString(response.Content.ReadAsStringAsync().Result);
+                body = (response.Content == null) ? string.Empty : FormatString(response.Content.ReadAsStringAsync().Result);
             }
             catch { }
 
@@ -60,7 +61,6 @@ namespace Microsoft.Graph.PowerShell
         {
             IList<Regex> regexList = new List<Regex>();
             // Remove access_token:*  instances in body.
-            Regex regexPattern = new Regex("(\\s*\"access_token\"\\s*:\\s*)\"[^\"]+\"");
             regexList.Add(regexPattern);
 
             foreach (Regex matcher in regexList)
